804cced04e91534f1c0c1fcdd339e7af3c614c5b
Aneeshahammed108 <aneeshahmd4@gmail.com>
2026-01-21 05:33:21 +0530

    first commit

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -66,7 +66,7 @@ const PROJECTS = [
       "A responsive e-commerce UI featuring product listings and reusable components.",
     tech: ["React", "CSS", "JavaScript"],
     image:
-      "/images/portfolio.jpeg",
+      "/images/E-commerce.avif",
     live: "#",
     github: "#",
   },
